20e0ddbc72ccf4831bfa5eb12af2a3cc3a494888
Tae hee <lte1807@gmail.com>
2024-08-25 21:49:33 +0900

    initail commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+//cors 설정
+app.use(cors());
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://therain0517.cafe24api.com/api/v2/admin/products",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer bP1Pm8pkB10kfxkPlM4muC",
+        },
+      }
+    );
+
+    // Cafe24 API의 응답을 클라이언트로 반환
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res
+      .status(500)
+      .json({ message: "Error fetching products", error: error.message });
+  }
+});
+
+// Express 서버 실행
+app.listen(port, () => {
+  console.log(`Proxy server is running on http://localhost:${port}`);
+});
+
+module.exports = app;
